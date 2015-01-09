b64246b26f2d72e115f5b4cb32b21690daa1f3e9
Ernesto Vargas <me@netoxico.com>
2015-01-09 11:46:43 -0600

    litle withe space

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ ensure_link ".dotfiles/vim/gvimrc"         ".gvimrc"
 ensure_link ".dotfiles/.gitexcludes"       ".gitexcludes"
 ensure_link ".dotfiles/.gitconfig"         ".gitconfig"
 ensure_link ".dotfiles/tmux/tmux.conf"     ".tmux.conf"
-ensure_link ".dotfiles/fish"   ".config/fish"
+ensure_link ".dotfiles/fish"               ".config/fish"
 ensure_link ".dotfiles/sublimetext/User/"  "Library/Application Support/Sublime Text 3/Packages/User"
 
 echo "[*] Installation completed"
